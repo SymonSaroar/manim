9385e0e1b9688162f7f3a4c9cfcc1205d00dde58
symonsaroar <saroarsymon@gmail.com>
2020-03-14 02:08:10 +0600

    added fractal

--- eanimation/DragonFractal.py
+++ eanimation/DragonFractal.py
@@ -7,33 +7,33 @@ else:
 
 class Dragon(MovingCameraScene):
     CONFIG = {
-        "iterations":18,
+        "iterations":5,
         "colors":[DARK_RED, YELLOW],
     }
     def construct(self):
         path = VGroup()
-        first_line = Line(ORIGIN,UP / 10).set_color(DARK_RED)
-        path.add(first_line)
+        line = Line(ORIGIN,UP / 10).set_color(DARK_RED)
+        path.add(line)
 
-        self.camera_frame.set_height(first_line.get_height() * 1.2)
-        self.camera_frame.move_to(first_line)
-        self.play(ShowCreation(first_line))
+        self.camera_frame.set_height(line.get_height() * 1.2)
+        self.camera_frame.move_to(line)
+        self.play(ShowCreation(line))
 
-        self.target_path = self.get_all_paths(path,self.iterations)
+        self.target_path = self.allP(path,self.iterations)
         for i in range(self.iterations):
             path.set_color_by_gradient(*self.colors)
-            self.duplicate_path(path,i)
+            self.dup(path,i)
         self.wait()
 
-    def duplicate_path(self,path,i):
+    def dup(self,path,i):
         set_paths = self.target_path[:2**(i + 1)]
         height = set_paths.get_height() * 1.1
-        new_path = path.copy()
-        self.add(new_path)
-        point = self.get_last_point(path)
+        new_P = path.copy()
+        self.add(new_P)
+        point = self.getP(path)
         self.play(
             Rotating(
-                new_path,
+                new_P,
                 radians=PI/2,
                 about_point=path[-1].points[point],
                 rate_func=linear
@@ -42,22 +42,22 @@ class Dragon(MovingCameraScene):
             self.camera_frame.set_height,height,
             run_time=1, rate_func=smooth
             )
-        post_path = reversed([*new_path])
-        path.add(*post_path)
+        newP = reversed([*new_P])
+        path.add(*newP)
 
-    def get_all_paths(self, path, iterations):
+    def allP(self, path, iterations):
         target_path = path.copy()
         for _ in range(iterations):
-            new_path = target_path.copy()
-            point = self.get_last_point(new_path)
-            new_path.rotate(
+            new_P = target_path.copy()
+            point = self.getP(new_P)
+            new_P.rotate(
                         PI/2, 
                         about_point=target_path[-1].points[point],
                     )
-            post_path = reversed([*new_path])
-            target_path.add(*post_path)
+            newP = reversed([*new_P])
+            target_path.add(*newP)
 
         return target_path
 
-    def get_last_point(self, path):
+    def getP(self, path):
         return 0 if len(path) > 1 else -1
